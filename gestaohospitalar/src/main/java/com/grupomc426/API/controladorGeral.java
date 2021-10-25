6e2abfb192fffb7ee6787c3a538dcf3aa97b4912
Melanie Soares <m241997@dac.unicamp.br>
2021-10-25 20:28:50 +0000

    Replace controladorGeral.java

--- gestaohospitalar/src/main/java/com/grupomc426/API/controladorGeral.java
+++ gestaohospitalar/src/main/java/com/grupomc426/API/controladorGeral.java
@@ -1,34 +1,70 @@
 package com.grupomc426.API;
 
+import java.util.List;
+import com.grupomc426.Targets.Atendimento.Exame;
+import com.grupomc426.Targets.Atendimento.Prontuario;
+import com.grupomc426.Targets.Produtos.Medicamento;
+import com.grupomc426.Targets.Usuarios.Medico;
+import com.grupomc426.Targets.Usuarios.Pessoa;
+import com.grupomc426.Targets.Atendimento.Agenda;
+import com.grupomc426.Targets.Atendimento.Horario;
+
 public class controladorGeral {
     private controladorHorarios controladorHorarios;
     private controladorProntuario controladorProntuario;
     private controladorConfirmacao controladorConfirmacao;
-    private static controladorGeral instance = null;
 
     public controladorGeral() {
+
         controladorHorarios = new controladorHorarios();
         controladorProntuario = new controladorProntuario();
         controladorConfirmacao = new controladorConfirmacao();
-
-        if(instance == null){
-            instance = this;
-        }
     }
 
-    public controladorGeral getInstance() {
-        return instance;
+
+    ///sub controlador prontuario
+    public boolean registrarConsulta(Medico medico, Pessoa pessoa) {
+        return this.controladorProntuario.registrarConsulta(medico, pessoa);
     }
 
-    public controladorHorarios getControladorHorarios() {
-        return controladorHorarios;
+    public boolean adicionarMedicamentos(String prontuarioID, List<Medicamento> medicamentos) {
+        return this.controladorProntuario.adicionarMedicamentos(prontuarioID, medicamentos);
     }
 
-    public controladorProntuario getControladorProntuario() {
-        return controladorProntuario;
+    public boolean adicionarExames(String prontuarioID, List<Exame> exames) {
+        return this.controladorProntuario.adicionarExames(prontuarioID, exames);
     }
-    
-    public controladorConfirmacao getControladorConfirmacao(){
-        return controladorConfirmacao;
+
+    public boolean assinarExame(String exameID, Medico medico) {
+        return this.controladorProntuario.assinarExame(exameID, medico);
     }
+
+    public Prontuario obterProntuario(String prontuarioID) {
+        return this.controladorProntuario.obterProntuario(prontuarioID);
+    }
+
+    //subcontrolador Horarios
+    public boolean registrarHorario(Medico medico, Horario horario) {
+        return this.controladorHorarios.registrarHorario(medico, horario);
+    }
+
+    public boolean reservarHorario(Pessoa pessoa, Horario horario) {
+
+        return this.controladorHorarios.reservarHorario(pessoa, horario);
+    }
+
+    public Agenda obterAgenda(Medico medico) {
+
+        return this.controladorHorarios.obterAgenda(medico);
+    }
+
+    //subcontrolador Confirmacao
+
+    public boolean confirmarHorario(Medico medico, Pessoa pessoa, Agenda agenda, Horario horario){
+        return this.controladorConfirmacao.confirmarHorario(medico, pessoa, agenda, horario);
+    }
+
+
+
+
 }
